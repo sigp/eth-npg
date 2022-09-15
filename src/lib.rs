cfa4dce65c8695e258ee17dda736e6884feb2526
Age Manning <Age@AgeManning.com>
2022-09-15 14:52:40 +1000

    Can't tell if better or worse, but i did some stuff that doesn't compile

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,69 @@ pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
-fn do_stuff(msg: PubsubMessage<MainnetEthSpec>) {
+
+fn dummy_sizes(msg: PubsubMessage<MainnetEthSpec>) -> PubsubMessage {
     match msg {
-        PubsubMessage::BeaconBlock(_) => todo!(),
-        PubsubMessage::AggregateAndProofAttestation(_) => todo!(),
-        PubsubMessage::Attestation(_) => todo!(),
-        PubsubMessage::VoluntaryExit(_) => todo!(),
-        PubsubMessage::ProposerSlashing(_) => todo!(),
-        PubsubMessage::AttesterSlashing(_) => todo!(),
+        PubsubMessage::BeaconBlock(_) => {
+            let large_block = BeaconBlockMerge::full(MainnetEthSpec::default_spec());
+            let signed_block = SignedBeaconBlock {
+                message: large_block,
+                signature: Signature::empty()
+            };
+            PubsubMessage::BeaconBlock(signed_block)
+        }
+        PubsubMessage::AggregateAndProofAttestation(_) => { 
+            let big_attestation = Attestation {
+            aggregation_bits: types::BitList::with_capacity(T::MaxValidatorsPerCommittee::to_usize()).unwrap(),
+            data: types::AttestationData::default(),
+            signature: types::AggregateSignature::empty(),
+            };
+            let big_aggregate_and_proof = types::AggregateAndProof<MainnetEthSpec> {
+                aggregator_index: 1_000_000,
+                aggregate: big_attestation,
+            }
+            let signed = SignedAggregateAndProof {
+                    message: big_aggregate_and_proof,
+                    signature: Signature::empty()
+            };
+            PubsubMessage::AggregateAndProof(signed)
+        },
+        PubsubMessage::Attestation(_) => {
+            let big_attestation = Attestation {
+            aggregation_bits: types::BitList::with_capacity(T::MaxValidatorsPerCommittee::to_usize()).unwrap(),
+            data: types::AttestationData::default(),
+            signature: types::AggregateSignature::empty(),
+            };
+            GossipMessage::Attestation(big_attestation)
+        }
+
+        PubsubMessage::VoluntaryExit(_) => {
+         let exit = VoluntaryExit {
+                    epoch: Epoch::new(1),
+                    validator_index: 1,
+                };
+        PubsubMessage::VoluntaryExit(exit)
+        }
+        PubsubMessage::ProposerSlashing(_) => { 
+        let header = BeaconBlockHeader {
+            slot: Slot::new(1),
+            proposer_index: 0,
+            parent_root: Hash256::zero(),
+            state_root: Hash256::zero(),
+            body_root: Hash256::zero(),
+        };
+
+        let signed_header = SignedBeaconBlockHeader {
+            message: header,
+            signature: Signature::empty(),
+        };
+        let proposer_slashing = ProposerSlashing {
+            signed_header_1: signed_header.clone(),
+            signed_header_2: signed_header,
+        };
+        PubsubMessage::ProposerSlashing(proposer_slashing)
+        }
+        PubsubMessage::AttesterSlashing(_) => todo!()
         PubsubMessage::SignedContributionAndProof(_) => todo!(),
         PubsubMessage::SyncCommitteeMessage(_) => todo!(),
     }
