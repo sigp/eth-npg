fc97cca26e3a235a7b69b5d9e6cfca5a388550ce
Diva M <divma@protonmail.com>
2022-09-20 18:08:01 -0500

    couple of type messages working on rate

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,11 @@
 use std::{
-    collections::VecDeque, convert::TryInto, marker::PhantomData, pin::Pin, sync::Arc, task::Poll,
+    collections::{HashSet, VecDeque},
+    convert::TryInto,
+    fmt::format,
+    marker::PhantomData,
+    pin::Pin,
+    sync::Arc,
+    task::Poll,
     time::Duration,
 };
 
@@ -34,47 +40,79 @@ pub enum MsgType {
 
 pub struct Generator<S, M> {
     slot_clock: S,
-    node_id: u64,
-    total_nodes: u64,
+    slots_per_epoch: u64,
+    validators: HashSet<u64>,
+    total_validators: u64,
     queued_messages: VecDeque<M>,
     next_slot: Pin<Box<Sleep>>,
 }
 
-impl<S: SlotClock> Generator<S, String> {
+type MT = String;
+
+impl<S: SlotClock> Generator<S, MT> {
     pub fn new(
         genesis_slot: Slot,
         genesis_duration: Duration,
+        slots_per_epoch: u64,
         slot_duration: Duration,
-        node_id: u64,
-        total_nodes: u64,
+        validators: HashSet<u64>,
+        total_validators: u64,
     ) -> Self {
         assert!(
-            node_id < total_nodes,
-            "nodes ids should go up to total_nodes - 1"
+            validators
+                .iter()
+                .max()
+                .map(|max_val_id| max_val_id < &total_validators)
+                .unwrap_or(true),
+            "validator ids should go up to total_validators - 1"
         );
-        println!("creating slot clock");
+        assert!(
+            total_validators >= 64 * 16,
+            "no idea what happens otherwise"
+        );
         let slot_clock = S::new(genesis_slot, genesis_duration, slot_duration);
-        println!("slot clock created");
         let duration_to_next_slot = slot_clock
             .duration_to_next_slot()
             .expect("nothing ever goes wrong");
-        println!("time to next slot: {duration_to_next_slot:?}");
         Generator {
             slot_clock,
-            node_id,
-            total_nodes,
+            slots_per_epoch,
+            validators,
+            total_validators,
             queued_messages: VecDeque::new(),
             next_slot: Box::pin(sleep(duration_to_next_slot)),
         }
     }
 
-    pub fn get_msg(&self, current_slot: Slot, kind: MsgType) -> Option<String> {
+    pub fn get_msg(&self, current_slot: Slot, kind: MsgType) -> Vec<MT> {
+        let slot = current_slot.as_u64();
         match kind {
             MsgType::BeaconBlock => {
-                return (current_slot % self.total_nodes == self.node_id)
-                    .then(|| format!("{kind}, {current_slot}"))
+                // return a block if we have the validator that should send a block
+                let proposer = slot % self.total_validators;
+                if self.validators.contains(&proposer) {
+                    vec![format!("{kind}, {current_slot} {proposer}")]
+                } else {
+                    vec![]
+                }
             }
-            // MsgType::AggregateAndProofAttestation => todo!(),
+            MsgType::AggregateAndProofAttestation => {
+                let epoch = current_slot.epoch(self.slots_per_epoch).as_u64();
+                self.validators
+                    .iter()
+                    .filter_map(|val_id| {
+                        // shake the val id using the epoch
+                        let shaked_val_id = val_id.overflowing_add(epoch).0;
+                        // assign to one of the committees
+                        let committee = shaked_val_id % 64;
+                        // get an id on the range of existing validator ids and use it to get an id
+                        // inside the committee
+                        let idx_in_commitee = (shaked_val_id % self.total_validators) / 64;
+                        let is_aggregator = idx_in_commitee / 16 == 0;
+                        is_aggregator.then(|| format!("val {val_id} aggregating on {committee}"))
+                    })
+                    .collect()
+            }
             // MsgType::Attestation => todo!(),
             // MsgType::VoluntaryExit => todo!(),
             // MsgType::ProposerSlashing => todo!(),
@@ -84,51 +122,51 @@ impl<S: SlotClock> Generator<S, String> {
             _ => {
                 let kind_: u64 = (kind as usize).try_into().unwrap();
                 if current_slot % 8 == kind_ {
-                    Some(format!("{kind}, {}", current_slot.as_u64()))
+                    vec![format!("{kind}, {}", current_slot.as_u64())]
                 } else {
-                    None
+                    vec![]
                 }
             }
         }
     }
 
     fn queue_slot_msgs(&mut self, current_slot: Slot) {
-        for msg_type in MsgType::iter() {
-            if let Some(msg) = self.get_msg(current_slot, msg_type) {
-                self.queued_messages.push_back(msg);
-            }
-        }
-        tracing::info!(
-            "[{current_slot}] Messages: len:{} {:?}",
-            self.queued_messages.len(),
-            self.queued_messages
-        );
+        //     for msg_type in MsgType::iter() {
+        //         if let Some(msg) = self.get_msg(current_slot, msg_type) {
+        //             self.queued_messages.push_back(msg);
+        //         }
+        //     }
+        //     tracing::info!(
+        //         "[{current_slot}] Messages: len:{} {:?}",
+        //         self.queued_messages.len(),
+        //         self.queued_messages
+        //     );
     }
 }
 
-impl<S: SlotClock + Unpin> Stream for Generator<S, String> {
-    type Item = String;
-
-    fn poll_next(
-        mut self: Pin<&mut Self>,
-        cx: &mut std::task::Context<'_>,
-    ) -> Poll<Option<Self::Item>> {
-        // If there were any messages remaining from the current slot, return them.
-        if let Some(msg) = self.queued_messages.pop_front() {
-            return Poll::Ready(Some(msg));
-        }
-
-        if self.next_slot.as_mut().poll(cx).is_ready() {
-            let current_slot = self.slot_clock.now().unwrap();
-            self.queue_slot_msgs(current_slot);
-
-            let duration_to_next_slot = self.slot_clock.duration_to_next_slot().unwrap();
-            tracing::debug!("Time to next slot {duration_to_next_slot:?}");
-            self.next_slot = Box::pin(sleep(duration_to_next_slot));
-            // We either have messages to return or need to poll the sleep
-            cx.waker().wake_by_ref();
-        }
-
-        Poll::Pending
-    }
-}
+// impl<S: SlotClock + Unpin> Stream for Generator<S, MT> {
+//     type Item = MT;
+//
+//     fn poll_next(
+//         mut self: Pin<&mut Self>,
+//         cx: &mut std::task::Context<'_>,
+//     ) -> Poll<Option<Self::Item>> {
+//         // If there were any messages remaining from the current slot, return them.
+//         if let Some(msg) = self.queued_messages.pop_front() {
+//             return Poll::Ready(Some(msg));
+//         }
+//
+//         if self.next_slot.as_mut().poll(cx).is_ready() {
+//             let current_slot = self.slot_clock.now().unwrap();
+//             self.queue_slot_msgs(current_slot);
+//
+//             let duration_to_next_slot = self.slot_clock.duration_to_next_slot().unwrap();
+//             tracing::debug!("Time to next slot {duration_to_next_slot:?}");
+//             self.next_slot = Box::pin(sleep(duration_to_next_slot));
+//             // We either have messages to return or need to poll the sleep
+//             cx.waker().wake_by_ref();
+//         }
+//
+//         Poll::Pending
+//     }
+// }
--- src/tests.rs
+++ src/tests.rs
@@ -3,18 +3,26 @@ use futures::StreamExt;
 use super::*;
 
 #[tokio::test]
-async fn test_generator() {
-    let subscriber = tracing_subscriber::FmtSubscriber::builder().finish();
-
-    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
-    let mut g = Generator::<SystemTimeSlotClock, String>::new(
+async fn test_expected_message_count_per_slot() {
+    // TODO: rand? quickcheck
+    let test_slot = Slot::new(8315647704);
+    const EXPECTED_BLOCKS: usize = 1;
+    const EXPECTED_AGGREGATES: usize = 16 * 64;
+    let total_validators = 16 * 64;
+    let all_validators = (0..total_validators).into_iter().collect::<HashSet<_>>();
+    let g = Generator::<SystemTimeSlotClock, String>::new(
         Slot::new(0),
         Duration::ZERO,
+        3,
         Duration::from_secs(2),
-        3, //node_id
-        5, //total_nodes
+        all_validators,
+        total_validators,
     );
-    while let Some(m) = g.next().await {
-        println!("{m}")
-    }
+    let blocks = g.get_msg(test_slot, MsgType::BeaconBlock);
+    let aggregators = g.get_msg(test_slot, MsgType::AggregateAndProofAttestation);
+    assert_eq!(blocks.len(), EXPECTED_BLOCKS);
+    assert_eq!(aggregators.len(), EXPECTED_AGGREGATES);
 }
+
+#[tokio::test]
+async fn test_timings() {}
