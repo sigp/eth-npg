74bc80800b26149c24a773ae92be312e8ee1320a
Age Manning <Age@AgeManning.com>
2022-12-05 14:12:45 +1100

    Update the sizes based off mainnet tests

--- src/sizes.rs
+++ src/sizes.rs
@@ -2,30 +2,38 @@ use crate::Message;
 use rand::Rng;
 
 impl Message {
-    // Is this somewhat believable?
+    // Tested from live mainnet results
     pub fn payload(&self) -> Vec<u8> {
         let mut rng = rand::thread_rng();
-        match self {
+        let mut message = match self {
             Message::BeaconBlock { .. } => {
                 let bytes: u32 = rng.gen_range(60_000..80_000);
                 vec![0; bytes as usize]
+                // Thread is some random values to make the payloads distinct
             }
             Message::AggregateAndProofAttestation { .. } => {
-                let bytes: u32 = rng.gen_range(80_000..135_000);
+                let bytes: u32 = rng.gen_range(500..550);
                 vec![0; bytes as usize]
             }
             Message::Attestation { .. } => {
-                let bytes: u32 = rng.gen_range(30_000..55_000);
+                let bytes: u32 = rng.gen_range(200..310);
                 vec![0; bytes as usize]
             }
             Message::SignedContributionAndProof { .. } => {
-                let bytes: u32 = rng.gen_range(3_000..8_000);
+                let bytes: u32 = rng.gen_range(410..430);
                 vec![0; bytes as usize]
             }
             Message::SyncCommitteeMessage { .. } => {
-                let bytes: u32 = rng.gen_range(8_000..20_000);
+                let bytes: u32 = rng.gen_range(190..210);
                 vec![0; bytes as usize]
             }
+        };
+
+        // Ranomize the first 8 bits to make sure the message is unique.
+        for index in 0..8 {
+            message[index] = rand::random();
         }
+
+        message
     }
 }
